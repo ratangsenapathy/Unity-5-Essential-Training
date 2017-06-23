e507927882b6e0eac24435582afe4bf5a334cbfc
ratansen <ratangsenapathy@gmail.com>
2017-06-23 11:40:57 -0700

    5 sections of training done

---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Engine : MonoBehaviour {
+
+	// Use this for initialization
+	public bool Chug;
+	private Animator animator;
+	private GameObject player;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator = GetComponent<Animator> ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		animator.SetBool ("Chug", true);
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		animator.SetBool ("Chug", false);
+	}
+}
