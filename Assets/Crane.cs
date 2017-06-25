a277343ad4162221140f53f2e74012844111a32f
ratansen <ratangsenapathy@gmail.com>
2017-06-24 20:05:22 -0700

    Learned level design and terrain design

---
+++ Assets/Crane.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crane : MonoBehaviour {
+	public bool Swung;	// Reference to the animator bool to trigger the state.
+
+	private Animator anim;		// Reference to the animator component.
+	private GameObject player;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		anim = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter (Collider other){
+		anim.SetBool("Swung", true);
+			}
+
+	void OnTriggerExit (Collider other){
+		anim.SetBool("Swung", false);
+	}
+}
---
+++ Assets/Doors.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Doors : MonoBehaviour {
+	public bool DoorCycle;	// Reference to the animator bool to trigger the state.
+	
+	private Animator anim;		// Reference to the animator component.
+	private GameObject player;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		anim = GetComponent<Animator>();
+	}
+	
+	void OnTriggerEnter (Collider other){
+		anim.SetBool("DoorCycle", true);
+	}
+	
+	void OnTriggerExit (Collider other){
+		anim.SetBool("DoorCycle", false);
+	}
+}
